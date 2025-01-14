bbdde1de1b51170bed9967f4585ec6a96fef1aa5
Jansiya jahan <jansiyajahan8@gmail.com>
2025-01-14 09:50:29 +0530

    fix the  create blog error addding the  end points

--- src/pages/Blog/CreateBlog.jsx
+++ src/pages/Blog/CreateBlog.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import * as yup from 'yup';
+import { createBlog } from "../../endpoints/useEndpoints";
 
 const schema = yup.object().shape({
   heading: yup
